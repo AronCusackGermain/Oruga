c478955abcc43ed49a4ff6d91ced6ca93bd5042e
AronCusackGermain <127886456+AronCusackGermain@users.noreply.github.com>
2026-01-27 10:45:33 -0300

    Delete gaming/src/main/java/com/oruga/gaming/dto directory

--- gaming/src/main/java/com/oruga/gaming/dto/request/CheckoutRequest.java
+++
@@ -1,9 +0,0 @@
-package com.oruga.gaming.dto.request;
-
-import lombok.Data;
-
-@Data
-public class CheckoutRequest {
-    // Por ahora vacío, se puede extender con más datos si es necesario
-    // como dirección de envío, método de pago alternativo, etc.
-}
